daefbb1429714d294dcead42a1ac8b8a3a767fb5
Fidel Hounkponou <fidhounk@gmail.com>
2022-01-17 13:07:25 -0500

    ddddd

--- salvaAuto/index.php
+++ salvaAuto/index.php
@@ -1,17 +1,6 @@
 <?php
 	require_once("partial/header.php");
 ?>
-
-	<div class="logoAuto"></div>
-	<div class="dom">Lave Auto à Domicile</div>
-	<div class="btn-container" ><button class="btn">Prenez un rendez-vous</button></div>
-	<div class="line-load">
-		<div class="line"></div>
-	</div>
-	<div>
-		
-	</div>
-	
 	
 
 <?php
--- salvaAuto/partial/header.php
+++ salvaAuto/partial/header.php
@@ -9,22 +9,4 @@
 </head>
 <body>
 
-    <div class="header" >
-
-        <!-- <div class="logo"></div> -->
-        <div class="salva"> 
-            <a href="" target="_blank"> <span>SALVA</span> </a>
-        </div>
-
-        <div class="middle">
-            <div class="swing"><a href="" target="_blank"> <span>Exterieur</span> </a></div>
-            <div class="swing"><a href="" target="_blank"> <span>Interieur</span> </a></div>
-            <div class="swing"><a href="" target="_blank"> <span>Complet</span> </a></div>
-            <div class="swing"><a href="" target="_blank"> <span>Maintenance</span> </a></div>
-        </div>
-
-        <div class="other">
-            <div class="swing"><a href="" target="_blank"> <span>Autres services</span> </a></div>
-        </div>
-
-    </div>
+    
